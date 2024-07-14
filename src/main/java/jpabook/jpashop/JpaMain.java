84be6aac12c387c6e33546102627469947839641
jaebinn <whiteclover129@gmail.com>
2024-07-14 21:35:21 +0900

    요구사항 분석 및 기본 매핑

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Order;
+
+import java.lang.reflect.Member;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        //persistence.xml에 있는 "hello" 가져다쓰기 위한 작업
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Order order = em.find(Order.class, 1L);
+            long memberId = order.getMemberId();
+
+            Member member = em.find(Member.class, memberId);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close(); // 닫아줘야 데이터베이스 컬랙션이 반환됨
+        }
+        emf.close();
+    }
+
+}
